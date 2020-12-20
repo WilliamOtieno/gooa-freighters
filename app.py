db131be90e712c01e15387be777ed00fa5a22859
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-12-21 01:33:28 +0300

    added services in app

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def hello_world():
     return render_template('index.html')
 
 
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+
 if __name__ == '__main__':
     app.run()
